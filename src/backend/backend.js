1c2e4849c3654636ffdcac466004146a9461581f
Quoc Khanh Khong <khanhquoc1125@gmail.com>
2025-03-23 15:46:47 +0700

    fix bugs: Cant sent large image

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -3,11 +3,7 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-import fs from 'fs';
-import path from 'path';
-import {fileURLToPath} from 'url';
 import { Buffer } from 'buffer';
-
 
 const app = express();
 const server = http.createServer(app);
@@ -15,32 +11,16 @@ const io = new Server(server, {
     cors: {
         origin: '*',
         methods: ['GET', 'POST'],
-    }
+    },
+    maxHttpBufferSize: 50e6 // Increase Socket.IO buffer size to 50MB
 });
 
 app.use(cors());
 // app.use(express.static('uploads')); // Serve uploaded images
-app.use(express.json({ limit: '10mb' })); // Allow large image payloads
+app.use(express.json({ limit: '50mb' })); // Increased limit for larger images
 
 let totalOnlineUser = 0;
 let userCount = 1;
-// function handleImage(data) {
-//     const imageBuffer = Buffer.from(data.message.split(',')[1], 'base64');
-//     const imageName = `image_${Date.now()}.png`;
-//     const imagePath = path.join('uploads', imageName);
-//
-//     fs.writeFile(imagePath, imageBuffer, (err) => {
-//         if (err) {
-//             console.error("Error saving image:", err);
-//             return;
-//         }
-//         io.emit('chat message', {
-//             username: data.username,
-//             message: `http://localhost:3000/${imageName}`,
-//             type: 'image'
-//         });
-//     });
-// }
 
 function handleImage(data) {
     console.log(`Received image from ${data.username}`);
@@ -58,13 +38,11 @@ io.on('connection', (socket) => {
 
     const username = `Anonymous user ${userCount++}`;
 
-
     //send message to client ( front end)
     socket.emit('assign username', username);
 
     console.log(`A user connected. Online users: ${totalOnlineUser}`);
     io.emit('user count', totalOnlineUser); // update to all users that someone connected
-
 
     //receive message from client ( front end)
     socket.on('chat message', (data) => {
@@ -78,7 +56,6 @@ io.on('connection', (socket) => {
             totalOnlineUser--;
         }
         io.emit('user count', totalOnlineUser); // update to all users that someone disconnected
-
     });
 });
 
@@ -87,4 +64,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
